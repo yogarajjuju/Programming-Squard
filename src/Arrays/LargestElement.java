f12ef19827d80d68bff780e5a0f132ef73073f1e
Yogaraj <yogarajjuju22@gmail.com>
2025-09-24 18:49:51 +0530

    First

--- src/Arrays/LargestElement.java
+++ src/Arrays/LargestElement.java
@@ -4,7 +4,7 @@ public class LargestElement {
     public static void main(String[] args) {
         int arr[] = {2,5,1,3,0};
 
-        // the brute way is to use inbuilt sort and print n-1
+        // the bruteforce way is to use inbuilt sort and print n-1
         // the optimal way is to use max variable and iterate the array
         int max =Integer.MIN_VALUE; // u we can use max=arr[0]
 
